d5c1ed9beb860b1d702fbbb8d5c6fd1139c43cae
Sudhanshu Dixit <109012761+SudhanshuDixit01@users.noreply.github.com>
2024-08-01 22:07:34 +0530

    Update ListingPage.js

--- frontend/src/components/ListingPage.js
+++ frontend/src/components/ListingPage.js
@@ -9,7 +9,7 @@ const ListingPage = () => {
   useEffect(() => {
     const fetchMedia = async () => {
       try {
-        const response = await axios.get(`http://localhost:5000/api/media`);
+        const response = await axios.get(`https://backend-rh85.onrender.com/api/media`);
         setMedia(response.data);
       } catch (error) {
         console.error(error);
